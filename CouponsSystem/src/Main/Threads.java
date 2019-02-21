e418b91afdd7fb6be5aed7fae8a096cf8d38e16f
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2019-02-21 21:22:46 +0200

    threads

---
+++ CouponsSystem/src/Main/Threads.java
@@ -0,0 +1,38 @@
+package Main;
+
+import java.util.Date;
+import java.util.Set;
+import Coupon.Coupon;
+import Coupon.CouponDBDAO;
+
+public class Threads implements Runnable {
+
+	private boolean exit = false;
+	private Date localDate;
+	private CouponDBDAO couponDBDAO = new CouponDBDAO();
+	private long id;
+
+	public void couponExpired() throws Exception {
+		Set<Coupon> coupons = couponDBDAO.getAllCoupon();
+		for (Coupon coupon : coupons) {
+			if (coupon.getEndDate().before(localDate))
+				id = coupon.getId();
+			couponDBDAO.removeCoupon(id);
+			// long id= coupon.getId();
+		}
+	}
+
+	@Override
+	public synchronized void run() {
+		// TODO Auto-generated method stub
+		while (!exit)
+			try {
+				this.couponExpired();
+				Thread.sleep(1000 * 60 * 60 * 24);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+	}
+
+}
