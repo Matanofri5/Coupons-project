7ded5c5d3fc6d828172b0b4157a649b9bc7962e9
Matanofri5 <45266810+Matanofri5@users.noreply.github.com>
2019-02-22 15:03:15 +0200

    commit

--- CouponsSystem/src/CustomerCoupon/CustomerCouponDBDAO.java
+++ CouponsSystem/src/CustomerCoupon/CustomerCouponDBDAO.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
 
 import Main.Database;
 
