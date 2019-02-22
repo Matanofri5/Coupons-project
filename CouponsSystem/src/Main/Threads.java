9f87a0f76af35484457473865f4a8d7ca3d421ba
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2019-02-22 10:53:48 +0200

    update thread

--- CouponsSystem/src/Main/Threads.java
+++ CouponsSystem/src/Main/Threads.java
@@ -11,24 +11,46 @@ public class Threads implements Runnable {
 	private Date localDate;
 	private CouponDBDAO couponDBDAO = new CouponDBDAO();
 	private long id;
+	
 
 	public void couponExpired() throws Exception {
+
 		Set<Coupon> coupons = couponDBDAO.getAllCoupon();
 		for (Coupon coupon : coupons) {
 			if (coupon.getEndDate().before(localDate))
 				id = coupon.getId();
 			couponDBDAO.removeCoupon(id);
 			// long id= coupon.getId();
+			System.out.println("this coupon has been delete: "+ couponDBDAO.getCoupon(id));
 		}
+		
+	}
+	
+	public void startThread() throws Exception{
+		Thread t1 = new Thread(this);
+		t1.start();
 	}
 
+//	@Override
+//	public synchronized void run() {
+//		// TODO Auto-generated method stub
+//		while (!exit)
+//			try {
+//				this.couponExpired();
+//				Thread.sleep(1000 * 60 * 60 * 24);
+//			} catch (Exception e) {
+//				System.out.println(e.getMessage());
+//			}
+//
+//	}
+	
 	@Override
 	public synchronized void run() {
 		// TODO Auto-generated method stub
 		while (!exit)
 			try {
 				this.couponExpired();
-				Thread.sleep(1000 * 60 * 60 * 24);
+				Thread.sleep(5000);
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
