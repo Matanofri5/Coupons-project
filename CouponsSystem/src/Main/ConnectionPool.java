1a1107d1070bc37cc5b3545d7b6b0a4d0b0a012c
Matanofri5 <45266810+Matanofri5@users.noreply.github.com>
2019-02-23 17:28:57 +0200

    added connectionpool class,not finished

---
+++ CouponsSystem/src/Main/ConnectionPool.java
@@ -0,0 +1,11 @@
+package Main;
+
+import java.util.ArrayList;
+
+public class ConnectionPool {
+
+	private static ConnectionPool instance;
+	private final int MaxConnnectionNumber=10;
+//	private ArrayList <connection> conn = new ArrayList<>(connection);
+
+}
