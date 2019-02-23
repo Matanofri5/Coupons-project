2ee4a0f21cde8a57080c75fe4fbe09472df0b5de
Matanofri5 <45266810+Matanofri5@users.noreply.github.com>
2019-02-23 17:02:18 +0200

    added Removecompany exception and changed name in creation exception

---
+++ CouponsSystem/src/MyExceptions/CompanyCreationException.java
@@ -0,0 +1,19 @@
+package MyExceptions;
+
+import Company.Company;
+
+public class CompanyCreationException extends Exception{
+
+	private Company company;
+	
+	public CompanyCreationException (Company company) {
+		this.company = company;
+	}
+	@Override
+	public String getMessage() {
+		return "Faild to create " + this.company.getCompanyName() + " company";    
+		}
+		
+		
+	}
+	
---
+++ CouponsSystem/src/MyExceptions/RemoveCompanyException.java
@@ -0,0 +1,17 @@
+package MyExceptions;
+
+import Company.Company;
+
+public class RemoveCompanyException {
+
+	private Company company;
+	
+	public RemoveCompanyException (Company company) {
+		this.company = company;
+	}
+	
+	public String getMessage () {
+		return "Faild to remove " + this.company.getCompanyName() + "company";
+	}
+	
+}
