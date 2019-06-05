0176d598a3ce451ed39f8b6f754fce71a9ef3aec
Matanofri5 <45266810+Matanofri5@users.noreply.github.com>
2019-06-05 20:26:13 +0300

    comments V

--- CouponsSystem/src/Clients/CouponClientFacade.java
+++ CouponsSystem/src/Clients/CouponClientFacade.java
@@ -1,5 +1,9 @@
 package Clients;
 
+/**
+ * @author Linoy & Matan
+ * @Description : interface class for logins to Admin, Company and Customer.
+ */
 public interface CouponClientFacade {
 
 	public CouponClientFacade login (String name, String password, ClientType clientType)throws Exception ;
